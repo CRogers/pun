0d8bd5d58291927446f792422db38ef61052a598
Callum Rogers <callumrogers@hotmail.co.uk>
2012-04-03 13:49:42 +0100

    Removed splat test from array test

--- tests/array.js
+++ tests/array.js
@@ -7,14 +7,11 @@
     return "onetwothree";
   }, [$, $('a'), $('b', Number)], function(first) {
     return "" + first + ", " + this.a + ", " + this.b;
-  }, [$, $(___)], function(h, t) {
-    return "Head: " + h + ", Tail: " + t;
   });
   console.log(f([0, "lol", 2]));
   console.log(f([0, "lol", true]));
   console.log(f([1, 2, 3]));
   console.log(f([1, 2, 3, 4]));
-  console.log(f([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
   /*<<
   0, lol, 2
   undefined
